92d9fb3bdf6759e0a34559d60786dc531bcd80ac
Repticus <martin.bubik@flashlink.cz>
2015-08-17 01:38:28 +0200

    added page Jidelni listek

--- app/presenters/WebPresenter.php
+++ app/presenters/WebPresenter.php
@@ -9,5 +9,8 @@ class WebPresenter extends Nette\Application\UI\Presenter {
 	public function actionTuristikaPribram() {
 		$this->template->tourism = $this->context->parameters['tourism'];
 	}
+	public function actionJidelniListek() {
+		$this->template->menu = $this->context->parameters['menu'];
+	}
 
 }
