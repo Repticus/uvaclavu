ab6dffbbefe4cba241db63853e8dd995721706db
Repticus <martin.bubik@flashlink.cz>
2015-10-23 01:36:28 +0200

    redesigned calendar component

--- app/component/Calendar.php
+++ app/component/Calendar.php
@@ -6,145 +6,121 @@ use Nette\Application\UI;
 
 class Calendar extends UI\Control {
 
+	private $firstDate;
+	private $requestDate;
+	private $event;
 	private $opentime;
 	private $calendar;
 
-	public function __construct($opentime, $calendar) {
+	public function __construct($opentime, $event, $date = NULL) {
 		parent::__construct();
+		$this->firstDate = strtotime(date('o-\\WW'));
+		$this->requestDate = $this->setDate($date);
+		$this->event = $event;
 		$this->opentime = $opentime;
-		$this->calendar = $this->setCalendar($calendar);
+		$this->calendar = $this->setCalendar();
 	}
 
 	public function render() {
 		$template = $this->template;
+		$this->template->days = array('Po', 'Út', 'St', 'Čt', 'Pá', 'So', 'Ne');
 		$this->template->calendar = $this->calendar;
 		$template->render(__DIR__ . '/Calendar.latte');
 	}
 
 	public function renderOpenTime() {
+		$seasonId = $this->getSeasonId($this->requestDate);
 		$template = $this->template;
-		$this->template->opentime = $this->opentime;
+		$this->template->days = array('Pondělí', 'Úterý', 'Středa', 'Čtvrtek', 'Pátek', 'Sobota', 'Neděle');
+		$this->template->opentime = $this->opentime[$seasonId]['days'];
+		$this->template->season = $this->getSeason($seasonId);
 		$template->render(__DIR__ . '/OpenTime.latte');
 	}
 
-	public function isTimeChanged($date) {
-		$day = date("N", $date);
-		$openTime = $this->calendar[$date]['time'];
-		$defaultOpenTime = $this->getDefaultOpenTime($day);
-		if ($openTime == $defaultOpenTime) {
-			return FALSE;
-		}
-		return TRUE;
-	}
-
-	private function getDefaultOpenTime($day) {
-		if (isset($this->opentime[$day]['open']) && isset($this->opentime[$day]['close'])) {
-			return array(
-				 'open' => $this->opentime[$day]['open'],
-				 'close' => $this->opentime[$day]['close']
-			);
-		}
-		return NULL;
-	}
-
-	public function getActiveOpenTime($date) {
-		if ($this->calendar[$date]['time']) {
-			return $this->calendar[$date]['time'];
-		} else {
-			return FALSE;
-		}
-	}
-
-	public function getEvent($date) {
-		if ($this->calendar[$date]['event']) {
-			return $this->calendar[$date]['event'];
-		} else {
-			return FALSE;
-		}
-	}
-
-	public function getAttr($date, $name) {
-		if (!$this->calendar[$date]) {
-			$attr = array('class' => 'c', 'title' => 'Zavřeno');
-		} elseif ($this->calendar[$date]['event']) {
-			$attr = array('class' => 'e', 'title' => $this->calendar[$date]['event']['name']);
-		} elseif ($this->isTimeChanged($date)) {
-			$attr = array('class' => 'd', 'title' => 'Změna otevírací doby');
-		} else {
-			$attr = array('class' => NULL, 'title' => 'Otevřeno');
-		}
-		if (isset($attr[$name])) {
-			return $attr[$name];
-		}
-		return FALSE;
-	}
-
-	private function setCalendar($calendar) {
-		$date = strtotime(date('o-\\WW'));
-		$data = array();
+	private function setCalendar() {
+		$date = $this->firstDate;
 		for ($week = 1; $week <= 5; $week++) {
 			for ($day = 1; $day <= 7; $day++) {
-				$data[$date] = $this->setDayData($date, $calendar);
+				$days[] = $date;
 				$date = strtotime("+1 day", $date);
 			}
 		}
-		return $data;
+		return $days;
 	}
 
-	private function setDayData($date, $calendar) {
-		$day = date("N", $date);
-		$open = $this->getOpenTime($day, $date, $calendar);
-		$closed = $this->getCloseTime($day, $date, $calendar);
-		$event = $this->setEvent($date, $calendar);
-		if ($open && $closed) {
-			return array(
-				 'time' => array('open' => $open, 'close' => $closed),
-				 'event' => $event
-			);
+	private function setDate($date) {
+		if ((int) $date < $this->firstDate) {
+			return strtotime("today");
+		} else {
+			return (int) $date;
+		}
+	}
+
+	private function getSeasonId($date) {
+		$month = (int) date("m", $date);
+		foreach ($this->opentime as $id => $season) {
+			if (in_array($month, $season['months'])) {
+				return $id;
+			}
 		}
 		return FALSE;
 	}
 
-	private function setEvent($date, $calendar) {
-		if (isset($calendar[$date]['event']['name']) && isset($calendar[$date]['event']['start'])) {
-			return $calendar[$date]['event'];
+	private function getSeason($seasonId) {
+		$months = $this->opentime[$seasonId]['months'];
+		$start = strftime('%B', mktime(0, 0, 0, $months[0], 1, 2000));
+		$end = strftime('%B', mktime(0, 0, 0, end($months), 1, 2000));
+		return array($start, $end);
+	}
+
+	private function getDefaultOpenTime($date) {
+		$day = date("N", $date) - 1;
+		$seasonId = $this->getSeasonId($date);
+		$days = $this->opentime[$seasonId]['days'];
+		if ($days[$day]) {
+			return $days[$day];
 		}
 		return FALSE;
 	}
 
-	private function getOpenTime($day, $date, $calendar) {
-		if (isset($calendar[$date]['open']) && !$calendar[$date]['open']) {
-			return FALSE;
-		}
-		$time = FALSE;
-		if (isset($calendar[$date]['event']['name']) && isset($calendar[$date]['event']['start'])) {
-			$time = $calendar[$date]['event']['start'];
-		}
-		if (isset($this->opentime[$day]['open'])) {
-			$time = $this->opentime[$day]['open'];
-		}
-		if (isset($calendar[$date]['open']) && $calendar[$date]['open']) {
-			$time = $calendar[$date]['open'];
-		}
-		if ($time) {
-			return $time;
+	public function getOpenTime($date) {
+		if (isset($this->event[$date]['open'])) {
+			if ($this->event[$date]['open']) {
+				return $this->event[$date]['open'];
+			}
+		} else {
+			return $this->getDefaultOpenTime($date);
 		}
 		return FALSE;
 	}
 
-	private function getCloseTime($day, $date, $calendar) {
-		if (isset($calendar[$date]['open']) && !$calendar[$date]['open']) {
-			return FALSE;
+	public function getEvent($date) {
+		if (isset($this->event[$date]['event'])) {
+			return $this->event[$date]['event'];
 		}
-		$time = FALSE;
-		if (isset($this->opentime[$day]['close'])) {
-			$time = $this->opentime[$day]['close'];
+		return FALSE;
+	}
+
+	public function getAttr($date) {
+		$opentime = $this->getOpenTime($date);
+		if (!$opentime) {
+			return array('class' => 'closed', 'title' => 'Zavřeno');
 		}
-		if (isset($calendar[$date]['close'])) {
-			$time = $calendar[$date]['close'];
+		$event = $this->getEvent($date);
+		if ($event) {
+			return array('class' => 'event', 'title' => $event[1]);
 		}
-		if ($time) {
-			return $time;
+		if ($this->isTimeChanged($date)) {
+			return array('class' => 'changed', 'title' => 'Změna otevírací doby');
+		}
+		return array('class' => NULL, 'title' => 'Otevřeno');
+	}
+
+	public function isTimeChanged($date) {
+		$openTime = $this->getOpenTime($date);
+		$defaultOpenTime = $this->getDefaultOpenTime($date);
+		if ($openTime <> $defaultOpenTime) {
+			return TRUE;
 		}
 		return FALSE;
 	}
