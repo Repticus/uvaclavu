f35263c8f06a986fa7685d088d7d68c0fcf4e2d2
Repticus <martin.bubik@flashlink.cz>
2016-02-01 01:53:04 +0100

    added head image animation

--- www/javascript.js
+++ www/javascript.js
@@ -1,4 +1,27 @@
 $(function () {
+	var delayInt = 3000;
+	var toggleInt = 2000;
+	setLight();
+	function setLight() {
+		$("#img1").delay(delayInt).fadeToggle(toggleInt, function () {
+			$("#img2").delay(delayInt).fadeToggle(toggleInt, function () {
+				$("#img3").delay(delayInt).fadeToggle(toggleInt, function () {
+					$("#img4").delay(delayInt).fadeToggle(toggleInt, function () {
+						$("#img5").delay(delayInt).fadeToggle(toggleInt, function () {
+							$("#img6").delay(delayInt).fadeToggle(toggleInt, function () {
+								$("#img1").delay(delayInt).fadeToggle(toggleInt, function () {
+									$("#img2").delay(delayInt).fadeToggle(toggleInt, function () {
+										$("#head img").fadeIn(0);
+										setLight();
+									});
+								});
+							});
+						});
+					});
+				});
+			});
+		});
+	}
 	$("#event").click(function (event) {
 		$("#eventScroll").slideToggle(1000);
 		$("#leaflet").slideToggle(1000);
